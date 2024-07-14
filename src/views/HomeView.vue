80b527eda89d8e86a68f273b9b523a8bd82591fa
belajar-vue-2 <aryaadi229@gmail.com>
2024-07-14 15:38:50 +0700

    Membuat Data pada Composition API

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 ref="heading">Hello World</h1>
+    <button @click="getEl">Click!</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const heading = ref(null);
+console.log(heading.value);
+
+const getEl = () => {
+  console.log(heading.value.classList.add("Heading"));
+  console.log(heading.value);
+  heading.value.textContent = "Hello Bro";
+};
+</script>
+
+<style scoped>
+</style>
