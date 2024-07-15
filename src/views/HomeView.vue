9fc46ac5933c663f3adcb6c31a4456dcb3b8a322
belajar-vue-2 <aryaadi229@gmail.com>
2024-07-15 19:16:24 +0700

    Memantau Perubahan Data Dengan Watch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,12 @@
     <div v-for="(name, index) in searchPerson" :key="index">
       {{ name }}
     </div>
+    <button @click="stopAll">Stop</button>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
 const search = ref("");
 const person = ref([
   "andrew",
@@ -23,6 +24,21 @@ const person = ref([
 const searchPerson = computed(() => {
   return person.value.filter((name) => name.includes(search.value));
 });
+
+const watchOne = watch(search, () => {
+  console.log("watch berjalan");
+});
+
+// Watch effect akan dijalankan pertama kali, dan akan mentrigger jika ada value yg dipantau berubah
+const watchEffectOne = watchEffect(() => {
+  console.log("watch effect berjalan", search.value);
+});
+
+// Untuk menghentikan watch dan watch effect
+function stopAll() {
+  watchOne();
+  watchEffectOne();
+}
 </script>
 
 <style scoped>
