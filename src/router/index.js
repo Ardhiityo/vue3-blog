86636b455e9e2f0dba9eed2b9f4164d3738ec3b6
belajar-vue-2 <aryaadi229@gmail.com>
2024-07-17 20:33:46 +0700

    Membuat Halaman Create Post dan Input Tags

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import {
 
 import HomeView from '../views/HomeView.vue'
 import ShowArticle from '../views/ShowArticle.vue'
+import Create from '../views/Create.vue';
 
 const router = createRouter({
   history: createWebHistory(
@@ -18,6 +19,11 @@ const router = createRouter({
       path: '/article/:id',
       name: 'article',
       component: ShowArticle
+    },
+    {
+      path: '/article/create',
+      name: 'create',
+      component: Create
     }
   ]
 })
---
+++ src/views/Create.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <form action="" method="post">
+      <label for="title">Title</label>
+      <br />
+      <input type="text" id="title" v-model="title" />
+      <br />
+
+      <label for="body">Body</label>
+      <br />
+      <input type="text" id="body" v-model="body" />
+      <br />
+
+      <label for="tag">Tag</label>
+      <br />
+      <input
+        type="text"
+        id="tag"
+        v-model="tag"
+        @keydown.enter.prevent="addTag"
+      />
+      <br />
+
+      <button>Create</button>
+
+      <div v-for="tag in tags" :key="tag">
+        {{ tag }}
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const title = ref("");
+const body = ref("");
+const tag = ref("");
+const tags = ref([]);
+
+function addTag() {
+  tag.value = "#" + tag.value.toLowerCase().replace(/\s+/g, "");
+  if (!tags.value.includes(tag.value)) {
+    tags.value.push(tag.value);
+  }
+  tag.value = "";
+}
+</script>
