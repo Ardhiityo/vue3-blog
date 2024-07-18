04340ad489f5b1bba1ca97edfcf948ed2b912347
belajar-vue-2 <aryaadi229@gmail.com>
2024-07-18 16:24:02 +0700

    Menginstall Library Pihak Ketiga Untuk Template Blog

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+// Bootstrap
 
+// Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+    FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome';
+import { fas } from "@fortawesome/free-solid-svg-icons";
+library.add(fas);
+// Font Awesome
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,34 @@
 <template>
-  <div>
+  <div class="container text-center">
+    <h1>Homepage</h1>
+    <p ref="para">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sed
+      dolores nostrum laudantium enim beatae nam quidem corporis veniam, saepe
+      officia laboriosam quis porro numquam velit, voluptates eveniet! Fugit,
+      magnam.
+    </p>
+
     <div v-if="loading">
       <Loading />
     </div>
 
     <div v-else>
-      <h1>Homepage</h1>
       <div v-if="err">{{ err }}</div>
       <PostList :posts="posts" />
     </div>
-    
   </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { onMounted, ref } from "vue";
 import Loading from "@/components/Loading.vue";
 import getPosts from "@/composable/getPosts";
 import PostList from "../components/PostList.vue";
 const { posts, err, load, loading } = getPosts();
+const para = ref(null);
 onMounted(() => {
+  console.log(para.value.innerText);
   load();
 });
 </script>
